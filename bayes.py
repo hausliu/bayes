87eb32738393e315bc35f8e820757957904b7e17
LiuHao321 <08183007@cumt.edu.cn>
2021-03-25 14:58:20 +0800

    just homework

--- bayes.py
+++ bayes.py
@@ -7,9 +7,8 @@ def loadCsv(filename):
     lines = csv.reader(open(filename, "r"))
     dataset = list(lines)
     for i in range(len(dataset)):
-        # dataset[i] = [float(x) for x in dataset[i][0:-1]]
-        dataset[i] = [float(x) for x in dataset[i]]
-        dataset[i][-1] = int(dataset[i][-1])
+        for j in range(len(dataset[i]) - 1):
+            dataset[i][j] = float(dataset[i][j])
     return dataset
 
 
@@ -100,19 +99,54 @@ def getAccuracy(testSet, predictions):
 
 
 def main():
-    filename = 'data_01.csv'
-    for splitRatio in range(50, 100):
-        # splitRatio = 0.67
-        splitRatio = splitRatio / 100
-        dataset = loadCsv(filename)
-        trainingSet, testSet = splitDataset(dataset, splitRatio)
-        print('Split {0} rows into train={1} and test={2} rows'.format(len(dataset), len(trainingSet), len(testSet)))
-        # prepare model
-        summaries = summarizeByClass(trainingSet)
-        # test model
-        predictions = getPredictions(summaries, testSet)
-        accuracy = getAccuracy(testSet, predictions)
-        print('splitRatio: {0}%, Accuracy: {1}%'.format(splitRatio, accuracy))
+    filename = 'data.csv'
+    splitRatio = 0.8
+    dataset = loadCsv(filename)
+    # 分割训练集和测试集
+    # trainingSet, testSet = splitDataset(dataset, splitRatio)
+    trainingSet = dataset
+    testSet = [[0.52, 0.8, 1.0, 0.0, 0.0, 5.0, 'SF'], [0.33, 0.5, 0.0, 0.0, 0.0, 1.0, 'PF'],
+               [0.47, 0.7, 2.0, 4.0, 1.0, 8.0, 'PG']]
+    print('Split {0} rows into train={1} and test={2} rows'.format(len(dataset), len(trainingSet), len(testSet)))
+    # dataset按标签类别进行分类，separated格式如下：{'c':[[样本1],[样本2],[样本3]],'SF':[[。。],[。。],[。。]]}
+    # 对separated字典计算每一类对应每列的均值和标准差
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+
+    y = []
+    for x in testSet:
+        y.append(x[-1])
+
+    separatedWithoutLable = separated.copy()
+    for key in separatedWithoutLable:
+        for l in separatedWithoutLable[key]:
+            l.pop()
+
+    # 计算统计量（statistics）均值（mean value）和标准差（standard deviation）
+    statistics = {}
+    for classValue, instances in separatedWithoutLable.items():  # dict_items([('C', [[1, 2, 3], [4, 5, 6], [7, 8, 9]]), ('D', [[3, 2, 1], [4, 3, 2], [5, 4, 3]])])
+        # listOfStatistics = [(sum(instances))]
+        statistics[classValue] = [(mean(a), stdev(a)) for a in zip(*instances)]
+
+    predictions = []
+    for i in range(len(testSet)):
+        res = predict(statistics, testSet[i])
+        predictions.append(res)
+
+    count = 0
+    for i in range(len(testSet)):
+        # print(y[i], predictions[i])
+        if y[i] == predictions[i]:
+            count += 1
+    accuracy = count / float(len(testSet))
+
+    # predictions = getPredictions(summaries, testSet)
+    # accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: {0}%'.format(accuracy))
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,23 +2,19 @@ import pandas as pd
 
 
 def make():
-    df = pd.read_csv("initData.csv", encoding='gbk')
+    df = pd.read_csv("newData.csv", encoding='gbk')
     df.dropna(inplace=True)
     print(df)
-    df.to_csv("gbkData.csv", index=False, sep=',')
+    df.to_csv("gbkData.csv", index=False, sep=',', encoding='utf-8')
     df = pd.read_csv("gbkData.csv")  # 先保存又重新读是为了解决编码问题，将编码格式为GBK的源数据转换为UTF-8的数据
     for c in range(0, df.shape[0]):
         if str(df.at[c, "Pos(位置)"]).find('-') != -1:
             df.at[c, "Pos(位置)"] = str(df.at[c, "Pos(位置)"])[:str(df.at[c, "Pos(位置)"]).find("-")]
     pos = df["Pos(位置)"]
-    df.drop(["Player", "Pos(位置)", "Age(年龄)", "Tm(球队)"], axis=1, inplace=True)
+    df.drop(["Pos(位置)"], axis=1, inplace=True)
     df.insert(df.shape[1], 'Pos(位置)', pos)
+    df.drop(index=[0], inplace=True)
     df.to_csv("data.csv", index=False, sep=',')
-    with open("data.csv") as f:
-        for lines in f:
-            lines = lines.replace('C', '0').replace('SF', '1').replace('SG', '2').replace('PG', '3').replace('PF', '4')
-            with open('data_01.csv', 'a') as a:
-                a.write(lines)
 
 
 if __name__ == '__main__':
